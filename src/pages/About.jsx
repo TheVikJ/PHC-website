e9a4afcb7f2eb72d7828d510c0c315e9ba8c70ff
Vikram <33194767+TheVikJ@users.noreply.github.com>
2021-07-15 16:37:40 +0530

    add text for About Me page 😎😎😎

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,22 +15,12 @@ function About(props) {
 				About us
 			</h1>
 			<p className={styles.heading}>
-				This is a heading
-			</p>
-			<p className={styles.subheading}>
-				This is a subheading
+				Welcome to PSN Hack Club!
 			</p>
 			<p className={styles.text}>
-				And this, is just a lot of text.
-			</p>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed vehicula sapien. Donec bibendum ullamcorper
-				risus, et tincidunt arcu pulvinar et. Maecenas quis massa ac sem porta iaculis. Praesent a bibendum elit. Morbi
-				nulla nibh, vestibulum sed nisl et, faucibus aliquam sem. Maecenas luctus tellus ac condimentum auctor. In diam
-				nisi, elementum vitae porta et, venenatis at ante. Donec ullamcorper eros et felis pellentesque iaculis. Sed
-				nisi justo, scelerisque vitae fermentum quis, gravida ut lacus. Nulla facilisi. Sed laoreet pulvinar vestibulum.
-				Praesent non nisl quis elit molestie interdum. In viverra pretium ligula vel sodales. Duis dictum ex mi,
-				pellentesque lobortis libero fringilla sed.
+				PSN Hack Club is a club under the global Hack Club network at Pathways School Noida. We're 
+				a welcoming community of programmers, developers and tech enthusiasts supporting each other on our
+				own journeys deeper into our respective fields. 
 			</p>
 		</div>
 	);
