a8d462b2d51221e7acd65d18404fde38f180bc69
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-07-19 16:38:02 +0530

    add sad panther

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import styles from '../stylesheets/Home.module.css';
-import {Link} from "react-router-dom";
+import React from "react";
+import styles from "../stylesheets/NotFound.module.css";
+import { Link } from "react-router-dom";
+import panther from "../images/common/sad_panther.png";
+import tinyPanther from "../images/common/tiny/sad_panther.png";
+
+import ProgressiveImage from "react-progressive-image";
 
 function NotFound(props) {
-	return (
-		<div className={styles.main}>
-			<h1 className={"header text-center"}>
-				404
-			</h1>
-			<p className={"text-content text-center"}>
-				The page you requested does not exist
-				<br/>
-				<Link to={'/'}>
-					Home
-				</Link>
-			</p>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <h1 className={"header text-center"}>404</h1>
+      <ProgressiveImage src={panther} placeholder={tinyPanther}>
+        {(src) => (
+          <img
+            alt={"A very sad panther :("}
+            src={src}
+            className={styles.panther}
+          />
+        )}
+      </ProgressiveImage>
+      <p className={"text-content text-center"}>
+        The page you requested does not exist
+        <br />
+        <Link to={"/"}>Home</Link>
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
