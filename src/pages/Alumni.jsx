6ce79d1bc73013ec76d6a15a71621d8c9e5f9b47
Dhruman Gupta <berlm.business@gmail.com>
2021-10-09 00:25:40 +0530

    fixed react/webpack/babel for being bad

--- src/pages/Alumni.jsx
+++ src/pages/Alumni.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from '../components/UI/Card'
 import styles from '../stylesheets/Alumni.module.css'
 import MetaDecorator from '../components/MetaDecorator'
-import { alumni, getImagePath } from '../data/Alumni'
+import { alumni } from '../data/Alumni'
 
 import ProgressiveImage from 'react-progressive-image'
 
@@ -11,7 +11,7 @@ const Alumni = () => (
     <MetaDecorator
       title={'PSN Hack Club | Alumni'}
       description={
-        "PSN Hack Club was founded in 2019. View all it's alumni here!"
+        'PSN Hack Club was founded in 2019. View all it\'s alumni here!'
       }
     />
 
@@ -20,22 +20,23 @@ const Alumni = () => (
         <p className={styles.yearHeader}>{year.year}</p>
         <div className={styles.alumniContainer}>
           {year.members.map((member) => {
+            const imagePath = `${member.name.toLowerCase().replaceAll(' ', '_')}.jpg`;
             let tinyImage
             let image
             try {
-              image = require(`../images/alumni/${getImagePath(
-                member.name
-              )}`).default
-            } catch {
+              image = require(`../images/alumni/${imagePath}`).default
+            } catch (e) {
+              console.log(e)
               image =
                 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg'
             }
 
             try {
-              tinyImage = require(`../images/alumni/tiny/${getImagePath(
-                member.name
-              )}`).default
-            } catch {}
+              tinyImage = require(`../images/alumni/tiny/${imagePath}`).default
+            } catch {
+            }
+
+            console.log(`${member.name}:\nnormal: ${image}\ntiny: ${tinyImage}`)
 
             return (
               <Card className={styles.alumni} key={member.name}>
