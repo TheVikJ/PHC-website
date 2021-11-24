7a01a800dabf3d064c6eb5005170c74819298f4c
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2021-11-25 01:40:47 +0530

    change alumni

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,24 +1,23 @@
 import React, {useState} from "react";
 import {SocialIcon as ReactSocialIcon} from "react-social-icons";
 
-function SocialIcon(props) {
-	const [hovering, setHovering] = useState(false);
-	const {colors, url} = props.data;
+function SocialIcon({ data }) {
+  const [hovering, setHovering] = useState(false)
 
-	const toggleHover = () => {
-		setHovering((prevState) => (!prevState));
-	};
+  const toggleHover = () => {
+    setHovering((prevState) => !prevState)
+  }
 
-	return (
-		<ReactSocialIcon
-			onMouseEnter={toggleHover}
-			onMouseLeave={toggleHover}
-			url={url}
-			fgColor={hovering ? colors.hover.fg : colors.fg}
-			bgColor={hovering ? colors.hover.bg : colors.bg}
-			target="_blank"
-		/>
-	)
+  return (
+    <ReactSocialIcon
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}
+      url={data.url}
+      fgColor={hovering ? data.colors.hover.fg : data.colors.fg}
+      bgColor={hovering ? data.colors.hover.bg : data.colors.bg}
+      target="_blank"
+    />
+  )
 }
 
 export default SocialIcon;
